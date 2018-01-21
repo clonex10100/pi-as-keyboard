3231ba8ae3bc97ff5932d55ff37af3677b88c646
Noah Evans <clonex10100@gmail.com>
2018-01-20 21:53:16 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 # Test if is Root
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-
+sleep 10
 for run in {1..10}
 do
 	echo -ne "\x00\x00\x00\x17\x00\x00\x00\x00" > /dev/hidg0
